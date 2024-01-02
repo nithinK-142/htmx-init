a4db1726e1b712cd8a884553bffa5ae47aa6d533
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-02 16:16:16 +0530

    add: GET request example.

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const router = express.Router();
+router.get("/users", async (req, res) => {
+  // const users = [
+  //   { id: 1, name: 'John Doe' },
+  //   { id: 2, name: 'Bob Williams' },
+  //   { id: 3, name: 'Shannon Jackson' },
+  // ];
+
+  setTimeout(async () => {
+    const limit = +req.query.limit || 10;
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
+    const users = await response.json();
+
+    res.send(`
+      <h1 class="text-2xl font-bold my-4">Users</h1>
+      <ul>
+        ${users.map((user) => `<li>${user.name}</li>`).join("")}
+      </ul>
+    `);
+  }, 1500);
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-import express from 'express';
+import express from "express";
+import usersRouter from "./routes/users.js";
 
 const app = express();
 
 // Set static folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use("/", usersRouter);
+
 // Start the server
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-  });
+  console.log("Server listening on port 3000");
+});
